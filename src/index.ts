0b88fb6d03d2dfc2a2bbd63e3946e49ede8c3dad
huanngo012 <huanngo1612@gmail.com>
2026-02-05 12:12:36 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+
+const port = process.env.PORT || 8888
+
+app.listen(port, () => {
+  console.log('Server running on the port: ' + port)
+})
